f1521de86ce66d6a75a7a070ce842d83338afe2c
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-05-31 12:09:30 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as an Auto Filter Bot if you like :D
+
+This bot is exclusively made for @pdxmoviesmp4
+
+You can see me in @pdxmoviesmp4 <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+<i>    Sorry you can't use me 😔,for movies join @pdxmoviesmp4
+</i>
+
+<b>© @pdxmoviesmp4</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : PDdX  BOT</b>
+    
+⭕️<b>Creater :</b> @G9037944937
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+⭕️<b>join channel :</b> <a href='https://t.me/pdxmoviesmp4'>click here</a> 
+
+"""
